8e5179af611977530d9679f83da7c76b86c13553
Akbarrahmadan <akbarrahmadan14@gmail.com>
2025-04-24 02:00:04 +0700

    Add files via upload

---
+++ Tugas pertemuan 7/TUgas akbarrahmadan 01sifk002.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std; int main()
+{
+double a=10;
+double b=9;
+double c=8;
+double x=7;
+double y=6;
+double result1=(-2+a+b)/c;
+double result2=sqrt((x/y)+pow(x,2));
+cout<<"hasil soal 1:"<<result1<<endl;
+cout<<"hasil soal 2:"<<result2<<endl;
+
+return 0;
+}
